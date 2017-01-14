59485fa522147d62667eecde0d53215304e8481b
Jakub Dmochowski <jakub@dmochowski.co>
2017-01-14 15:37:03 +0100

    Delete Placement.h

--- Placement.h
+++
@@ -1,8 +0,0 @@
-#ifndef _PLACEMENT_
-    #define _PLACEMENT_
-
-void Placement(int player);
-void placePenguin(int player, int x, int y);
-int placementValid(int x, int y);
-int countPossiblePlacements();
-#endif // _PLACEMENT_
